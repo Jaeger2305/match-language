8c83105f8d11d72839bef4a7e2b7713696110fe7
Jaeger2305 <rwebb2305@gmail.com>
2021-05-05 22:14:48 +0200

    feat: add match modal placeholder

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -1,9 +1,13 @@
 import './index.css';
 import Card from '../card'
+import MatchModal from '../match-modal'
 
 export default function game() {
   return (
     <div className="App">
+      <div className="match-modal">
+        <MatchModal />
+      </div>
       <div className="play-area">
         <Card />
         <Card />
---
+++ src/components/match-modal/index.tsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+export default function matchModal() {
+  return (
+    <div className="modal">
+      <div className="match-name reflected">Stuff</div>
+      <div className="match-name">Stuff</div>
+    </div>
+  );
+}
