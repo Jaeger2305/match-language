1903a4020cff2781621f8c21fecaa8b920393728
Jaeger2305 <rwebb2305@gmail.com>
2021-05-08 03:09:47 +0200

    feat: add basic matching logic

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,11 +1,31 @@
-import { Card } from '../../game';
-import './index.css';
+import {Card, Matchable} from "../../game";
+import "./index.css";
 
-export default function card(props: {card: Card}) {
-  const matchablesNodes = props.card.matchables.map(({ translationKey }) => <div className="matchable">{translationKey}</div>)
-  return (
-    <div className="card">
-      {matchablesNodes}
+export default function card({
+  card,
+  moveCard,
+  matchingCard,
+}: {
+  card: Card;
+  moveCard?: (card: Card) => void;
+  matchingCard?: (source: Card, matchable: Matchable) => Card | null;
+}) {
+  function attemptMatch(translationKey: Matchable): void {
+    if (!moveCard || !matchingCard) return;
+    const match = matchingCard(card, translationKey);
+    if (match) {
+      moveCard(match);
+    }
+  }
+
+  const matchablesNodes = card.matchables.map((matchable) => (
+    <div
+      className="matchable"
+      key={matchable.translationKey}
+      onClick={() => attemptMatch(matchable)}
+    >
+      {matchable.translationKey}
     </div>
-  );
+  ));
+  return <div className="card">{matchablesNodes}</div>;
 }
--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -4,7 +4,7 @@ import MatchModal from "../match-modal";
 import Stats from "../stats";
 import {Language} from "../../localisation";
 import {ReactNode, useState} from "react";
-import {Card, cards} from "../../game";
+import {Card, cards, Matchable} from "../../game";
 import {shuffle} from "lodash";
 
 const primaryLanguage = Language.English;
@@ -16,12 +16,39 @@ export default function Game() {
   const [player2Hand, setPlayer2Hand] = useState([gameCards.pop()!]);
   const [deck, setDeck] = useState(gameCards);
 
+  function matchingCard(
+    source: Card,
+    destination: Card,
+    matchable: Matchable
+  ): Card | null {
+    return destination.matchables.find(
+      ({translationKey}) => translationKey === matchable.translationKey
+    )
+      ? destination
+      : null;
+  }
+
+  function moveCard(
+    source: Array<Card>,
+    sourceSetter: React.Dispatch<React.SetStateAction<Card[]>>,
+    destination: Array<Card>,
+    destinationSetter: React.Dispatch<React.SetStateAction<Card[]>>,
+    card: Card
+  ): void {
+    const newSource = source.filter(({id}) => id !== card.id);
+    sourceSetter(newSource);
+    const newDestination = destination.concat(card);
+    destinationSetter(newDestination);
+  }
+
   function renderCardStack(stack: Array<Card>): ReactNode {
     if (stack.length) {
-      return <CardComponent card={stack.slice(-1)[0]} />
+      return <CardComponent card={stack.slice(-1)[0]} />;
     }
 
-    return <div style={{background: "black"}} />
+    return (
+      <div style={{background: "black", height: "100px", width: "100px"}} />
+    );
   }
 
   return (
@@ -31,13 +58,27 @@ export default function Game() {
       </div>
       <div className="play-area"></div>
       <div className="player-1-area">
-        {renderCardStack(player1Hand)}
+        <CardComponent
+          card={player1Hand.slice(-1)[0]}
+          moveCard={(card: Card) =>
+            moveCard(deck, setDeck, player1Hand, setPlayer1Hand, card)
+          }
+          matchingCard={(source: Card, matchable: Matchable) =>
+            matchingCard(source, deck.slice(-1)[0], matchable)
+          }
+        />
       </div>
-      <div className="shared-area">
-        {renderCardStack(deck)}
-      </div>
+      <div className="shared-area">{renderCardStack(deck)}</div>
       <div className="player-2-area">
-        {renderCardStack(player2Hand)}
+        <CardComponent
+          card={player2Hand.slice(-1)[0]}
+          moveCard={(card: Card) =>
+            moveCard(deck, setDeck, player2Hand, setPlayer2Hand, card)
+          }
+          matchingCard={(source: Card, matchable: Matchable) =>
+            matchingCard(source, deck.slice(-1)[0], matchable)
+          }
+        />
       </div>
       <div className="stats">
         <Stats />
