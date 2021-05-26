bb1d7b40aad8f3caf782f40d94f9a30adeeaa325
Jaeger2305 <rwebb2305@gmail.com>
2021-05-27 00:03:53 +0200

    feat: improve positioning algorithm
    simplify it and make it radial

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,7 +1,86 @@
 import {Card, Matchable} from "../../game";
 import "./index.css";
 
-export default function card({
+type Position = {
+  x: number;
+  y: number;
+};
+
+// generate random positions
+function generatePositionsArray(
+  diameter = 230,
+  divisions = 3,
+  irregularity = 10,
+  offset = 40,
+  curvatureStrength = 100
+): Array<Position> {
+  // declarations
+  const positionsArray = [];
+  var r, c;
+  const separationPixels = diameter / divisions;
+  // loop through rows
+  for (r = 0; r < divisions; r += 1) {
+    const isOverOrEqualHalfWayRow = r + 1 > Math.floor(divisions / 2);
+    const distanceToBottomEdge = isOverOrEqualHalfWayRow
+      ? divisions - r - 1
+      : r;
+    const curvatureRow = distanceToBottomEdge / divisions;
+    // loop through columns
+    for (c = 0; c < divisions; c += 1) {
+      const isOverOrEqualHalfWayCol = c + 1 > Math.floor(divisions / 2);
+      const distanceToRightEdge = isOverOrEqualHalfWayCol
+        ? divisions - c - 1
+        : c;
+      const curvatureCol = distanceToRightEdge / divisions;
+      const curvatureRowOffset =
+        curvatureRow * curvatureStrength * (isOverOrEqualHalfWayCol ? 1 : -1);
+      const curvatureColOffset =
+        curvatureCol * curvatureStrength * (isOverOrEqualHalfWayRow ? 1 : -1);
+      const isTopOrBotMid = !curvatureRow && curvatureCol;
+      const isLeftOrRightMid = curvatureRow && !curvatureCol;
+
+      // populate array with point object
+      const position = {
+        x:
+          Math.round(separationPixels * c) +
+          offset +
+          (isLeftOrRightMid ? curvatureRowOffset : 0) +
+          getRandomInt(irregularity * -1, irregularity),
+        y:
+          Math.round(separationPixels * r) +
+          offset +
+          (isTopOrBotMid ? curvatureColOffset : 0) +
+          getRandomInt(irregularity * -1, irregularity),
+      };
+      positionsArray.push(position);
+    }
+  }
+  // return array
+  return positionsArray;
+}
+
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + 0.5) + min;
+}
+
+function getRandomPosition(array: Array<Position>): Position {
+  // declarations
+  var randomIndex;
+  var coordinates;
+  // get random index
+  randomIndex = getRandomInt(0, array.length - 1);
+  // get random item from array
+  coordinates = array[randomIndex];
+  // check if remove taken
+  // remove element from array
+  array.splice(randomIndex, 1);
+  // return position
+  return coordinates;
+}
+
+export default function CardComponent({
   card,
   flashMatchModal,
   moveCard,
@@ -21,75 +100,10 @@ export default function card({
     }
   }
 
-  type Position = {
-    x: number;
-    y: number;
-  };
-
-  var positions: Array<Position> = [];
-
-  // Returns a random integer between min (included) and max (excluded)
-  // Using Math.round() will give you a non-uniform distribution!
-  function getRandomInt(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-  // generate random positions
-  function generatePositionsArray(
-    maxX: number,
-    maxY: number,
-    safeRadius: number,
-    irregularity: number
-  ): Array<Position> {
-    // declarations
-    var positionsArray = [];
-    var r, c;
-    var rows;
-    var columns;
-    // count the amount of rows and columns
-    rows = Math.floor(maxY / safeRadius);
-    columns = Math.floor(maxX / safeRadius);
-    // loop through rows
-    for (r = 1; r <= rows; r += 1) {
-      // loop through columns
-      for (c = 1; c <= columns; c += 1) {
-        // populate array with point object
-        positionsArray.push({
-          x:
-            Math.round((maxX * c) / columns) +
-            getRandomInt(irregularity * -1, irregularity),
-          y:
-            Math.round((maxY * r) / rows) +
-            getRandomInt(irregularity * -1, irregularity),
-        });
-      }
-    }
-    // return array
-    return positionsArray;
-  }
-  positions = generatePositionsArray(220, 220, 60, 15);
-  function getRandomPosition(
-    array: Array<Position>,
-    removeTaken: boolean
-  ): Position {
-    // declarations
-    var randomIndex;
-    var coordinates;
-    // get random index
-    randomIndex = getRandomInt(0, array.length - 1);
-    // get random item from array
-    coordinates = array[randomIndex];
-    // check if remove taken
-    if (removeTaken) {
-      // remove element from array
-      array.splice(randomIndex, 1);
-    }
-    // return position
-    return coordinates;
-  }
+  const positions = generatePositionsArray();
 
   const matchablesNodes = card.matchables.map((matchable) => {
-    const pos = getRandomPosition(positions, true);
+    const pos = getRandomPosition(positions);
 
     return (
       <img
